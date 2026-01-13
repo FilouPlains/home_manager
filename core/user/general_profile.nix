15aa7f25e528cf61e73d3b8221553dc58d2d7e4f
rouaud <rouaud@ibpc.fr>
2026-01-13 15:49:05 +0100

    Correcting packages issues.

--- core/user/general_profile.nix
+++ core/user/general_profile.nix
@@ -1,29 +1,33 @@
-{
-  config,
-  pkgs,
-  ...
-}: {
+{pkgs, ...}: {
   imports = [
     ../package/general_setup.nix
   ];
   # Do not change this value.
   # If you want to do so, check `Home Manager release notes`.
-  home.stateVersion = "24.05";
+  home = {
+    stateVersion = "24.05";
 
-  home.sessionVariables = {
-    # C
-    COLORTERM = "truecolor";
+    sessionVariables = {
+      # C
+      COLORTERM = "truecolor";
 
-    # E
-    EDITOR = "nvim";
+      # E
+      EDITOR = "nvim";
 
-    # M
-    MANROFFOPT = "-P -c";
-    MANPAGER = "nvimpager";
+      # M
+      MANROFFOPT = "-P -c";
+      MANPAGER = "nvimpager";
 
-    # P
-    PAGER = "nvimpager";
-    PYTHONPYCACHEPREFIX = "$HOME/.cache/cpython/";
+      # P
+      PAGER = "nvimpager";
+      PYTHONPYCACHEPREFIX = "$HOME/.cache/cpython/";
+    };
+
+    packages = with pkgs; [
+      bat
+      fastfetch
+      grc
+    ];
   };
 
   # Let Home Manager install and manage itself.
