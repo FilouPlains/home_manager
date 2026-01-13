bddfd9ce19227aeb856cf84513f23ad171450840
rouaud <rouaud@ibpc.fr>
2026-01-13 14:30:16 +0100

    Correcting home-manager version.

--- flake.nix
+++ flake.nix
@@ -3,10 +3,11 @@
 
   inputs = {
     # Increment release branch for NixOS
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
     home-manager = {
       # Follow corresponding `release` branch from Home Manager
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
