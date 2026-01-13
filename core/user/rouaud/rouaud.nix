686007526cfa61347c0deda3c6fbe3f41b4b6bab
rouaud <rouaud@ibpc.fr>
2026-01-13 15:34:31 +0100

    Adding me, because I'm so important… :)

---
+++ core/user/rouaud/rouaud.nix
@@ -0,0 +1,14 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    ../general_profile.nix
+  ];
+
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "rouaud";
+  home.homeDirectory = "/home/rouaud";
+}
